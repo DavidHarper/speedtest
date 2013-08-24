8e817ecc8f179528afebef6f82ad764f4c599a5d
David Harper <david@obliquity.net>
2013-08-24 14:02:13 +0000

    Added GPLv3 copyright notices to all files.  Added a copy of the GPLv3 license to the repository.
    git-svn-id: file:///disk2/repository/svn/speedtest/trunk@1167 32807195-9110-437f-9ebb-0d7c4c47859f

--- speedtest.c
+++ speedtest.c
@@ -1,3 +1,23 @@
+/***********************************************************************
+speedtest : a CPU speed test utility
+
+Copyright (C) 2013 David Harper at obliquity.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+***********************************************************************/
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/times.h>
@@ -8,6 +28,14 @@
 #include "vectorops.h"
 
 static void printUsage(FILE *fp, char *message) {
+  fprintf(fp, "speedtest ; a CPU speed test utility\n");
+  fprintf(fp, "\n");
+  fprintf(fp, "Copyright (C) 2013 David Harper at obliquity.com\n\n");
+  fprintf(fp, "This program comes with ABSOLUTELY NO WARRANTY.\n\n");
+  fprintf(fp, "This is free software, and you are welcome to redistribute it\n");
+  fprintf(fp, "under certain conditions.  Please refer to the GNU General\n");
+  fprintf(fp, "Public License V3 [http://www.gnu.org/licenses/].\n\n");
+
   if (message)
     fprintf(fp, "An error occurred: %s\n\n", message);
 
